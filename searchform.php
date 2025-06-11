1838ec85ce1fc62d9392d66c1050761c8e6ffe42
NomadPub <NomadPub@users.noreply.github.com>
2025-06-11 15:15:17 -0400

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
     <label>
         <span class="screen-reader-text">Search for:</span>
-        <input type="search" class="search-field" placeholder="Search MySpace..." value="<?php echo get_search_query(); ?>" name="s" />
+        <input type="search" class="search-field" placeholder="Search MySite..." value="<?php echo get_search_query(); ?>" name="s" />
     </label>
     <input type="submit" class="search-submit" value="Search" />
 </form>
